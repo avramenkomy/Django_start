359a20955bf204e478ed9782e6b624a61668c101
avramenkomy <67018344+avramenkomy@users.noreply.github.com>
2021-01-12 13:08:45 +0300

    Delete django_auth directory

--- django_auth/common/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from common.models import UserProfile
-
-# Register your models here.
-@admin.register(UserProfile)
-class ProfileAdmin(admin.ModelAdmin):
-    pass
--- django_auth/common/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from common.models import UserProfile
-
-class ProfileCreationForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['age']
