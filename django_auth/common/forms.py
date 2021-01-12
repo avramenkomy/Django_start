929e69844f8457d2774a378c9e5ffaacc8b5d2cb
Mikhail Avramenko <avramenkomy@yandex.ru>
2021-01-12 12:44:41 +0300

    master: initial commit

---
+++ django_auth/common/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from common.models import UserProfile
+
+class ProfileCreationForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['age']
